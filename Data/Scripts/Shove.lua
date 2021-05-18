3cf938f8570fa0ae19e7d46018293b79ce6ab1b2
Tom Austin <Tom.Austin@streets-heaver.com>
2021-05-18 08:13:45 +0100

    Tweaked raycast distance

--- Data/Scripts/Shove.lua
+++ Data/Scripts/Shove.lua
@@ -7,7 +7,7 @@ function OnCast(ability)
 
     local rayStart = player:GetWorldPosition() 
     local cameraForward = player:GetWorldRotation() * Vector3.FORWARD
-    local rayEnd = rayStart + cameraForward * 500
+    local rayEnd = rayStart + cameraForward * 200
 
     local hitResult = World.Raycast(rayStart, rayEnd, {
         ignorePlayers = player
